ab355129e4fb7c1944ef8c27d82ff3e9ea37ab2f
Luiz henrique de almeida santos <luizcoffebrake@gmail.com>
2026-06-14 15:37:53 -0300

    Sincronizado com o repositório da Joice e mantendo configurações locais

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,7 +7,7 @@ import { Platform } from 'react-native';
 
 const baseURL = Platform.OS === 'web'
   ? 'http://localhost:8080/api'
-  : 'http://192.168.1.4:8080/api'; // ← troque pelo seu IP (ipconfig → IPv4)
+git status  : 'http://192.168.1.4:8080/api'; // ← troque pelo seu IP (ipconfig → IPv4)
 
 export const api = axios.create({
   baseURL,
